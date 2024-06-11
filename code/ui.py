ccdae6dbd462544c96d855a0042889365aae47d3
unknown <antonio_martinez88@hotmail.com>
2024-06-11 12:08:53 -0400

    added basic drawing of health bar as black

--- code/ui.py
+++ code/ui.py
@@ -3,9 +3,14 @@ from settings import *
 
 class UI:
     def __init__(self):
+
         #general information
         self.display_surface = pygame.display.get_surface()
-        self.font
+        self.font = pygame.font.Font(UI_FONT, UI_FONT_SIZE)
+
+        # bar setup
+        self.health_bar_rect = pygame.Rect(10, 10, HEALTH_BAR_WIDTH, BAR_HEIGHT) #needs left, top, width, height
+        self.energy_bar_rect = pygame.Rect(10,34,ENERGY_BAR_WIDTH, BAR_HEIGHT)
 
     def display(self, player):
-        pass
+        pygame.draw.rect(self.display_surface, 'black', self.health_bar_rect) #needs surface, color, rectangle

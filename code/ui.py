f7f9995f0a9f33e08a1bc43b4e4acf37a7bad1ac
unknown <antonio_martinez88@hotmail.com>
2024-06-11 12:28:06 -0400

    added experience to the UI

--- code/ui.py
+++ code/ui.py
@@ -27,7 +27,17 @@ class UI:
         pygame.draw.rect(self.display_surface, UI_BORDER_COLOR, bg_rect, 3)
 
 
+    def show_exp(self, exp):
+        text_surf = self.font.render(str(int(exp)), False, TEXT_COLOR) #information, ANti-Aliasing, color
+        x = self.display_surface.get_size()[0] -20
+        y = self.display_surface.get_size()[1] - 20
+        text_rect = text_surf.get_rect(bottomright = (x,y))
+
+        self.display_surface.blit(text_surf, text_rect)
+
     def display(self, player):
         #pygame.draw.rect(self.display_surface, 'black', self.health_bar_rect) #needs surface, color, rectangle
         self.show_bar(player.health, player.stats['health'], self.health_bar_rect, HEALTH_COLOR)
         self.show_bar(player.energy, player.stats['energy'], self.energy_bar_rect, ENERGY_COLOR)
+
+        self.show_exp(player.exp)

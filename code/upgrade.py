e823ed4c6417cfe57996fdcdbde505ca97098823
unknown <antonio_martinez88@hotmail.com>
2024-06-18 14:17:08 -0400

    upd

--- code/upgrade.py
+++ code/upgrade.py
@@ -84,15 +84,21 @@ class Item:
         self.font = font
 
     def display_names(self, surface, name, cost, selected):
-        #title
-        title_surf = self.font.render(name, False, TEXT_COLOR) #creation
-        title_rect = title_surf.get_rect(midtop = self.rect.midtop+pygame.math.Vector2(0,20)) #rectangle where text gonna be
+        # title
+        title_surf = self.font.render(name, False, TEXT_COLOR)  # creation
+        title_rect = title_surf.get_rect(
+            midtop=self.rect.midtop + pygame.math.Vector2(0, 20)
+        )  # rectangle where text gonna be
 
-        #cost
+        # cost
+        cost_surf = self.font.render(f"{int(cost)}", False, TEXT_COLOR)  # creation
+        cost_rect = cost_surf.get_rect(
+            midbottom=self.rect.midbottom - pygame.math.Vector2(0, 20)
+        )
 
-        #draw
+        # draw
         surface.blit(title_surf, title_rect)
-
+        surface.blit(cost_surf, cost_rect)
 
     def display(self, surface, selection_num, name, value, max_value, cost):
         pygame.draw.rect(surface, UI_BG_COLOR, self.rect)
